4e15768f65997d62e5450c9f407b0dead767ea43
TakanoriOnuma <s1000607@sendai-nct.jp>
2016-09-10 11:40:26 +0900

    スイングごとのデータもグラフ化
    100スイング分のデータもグラフ化することは出来たが、
    gnuplotを100回呼び出すことになり、その度に窓が開いて
    時間がかかってしまった。
    gnuplot内部でループを回す必要があるかもしれない。
    
    Signed-off-by: TakanoriOnuma <s1000607@sendai-nct.jp>

--- arrangeProgram/script.py
+++ arrangeProgram/script.py
@@ -25,9 +25,19 @@ for dirName in glob.glob('part*'):
 
     # 中間層のニューロンの出力をグラフに表示する
     mkdir('{}/{}/outNeuron'.format(ROOT, dirName))
+    # trainディレクトリの記録
     mkdir('{}/{}/outNeuron/train'.format(ROOT, dirName))
     for viewDir in ['view0,0', 'view90,0', 'view90,90', 'view45,45']:
         mkdir('{}/{}/outNeuron/train/{}'.format(ROOT, dirName, viewDir))
-    arg = "path='{}/{}'; subDirName='{}'; limit={}".format(ROOT, dirName, 'train', 10000)
+    arg = "path='{}/{}'; subDirName='train'; limit={}".format(ROOT, dirName, 10000)
     exeName = 'gnuplot/3DNeuron.gp'
     doGnuplot(arg, exeName)
+    # testディレクトリの記録
+    mkdir('{}/{}/outNeuron/test'.format(ROOT, dirName))
+    for swingNum in range(100):
+        mkdir('{}/{}/outNeuron/test/{}'.format(ROOT, dirName, swingNum))
+        for viewDir in ['view0,0', 'view90,0', 'view90,90', 'view45,45']:
+            mkdir('{}/{}/outNeuron/test/{}/{}'.format(ROOT, dirName, swingNum, viewDir))
+        arg = "path='{}/{}'; subDirName='test/{}'; limit={}".format(ROOT, dirName, swingNum, 10000)
+        exeName = 'gnuplot/3DNeuron.gp'
+        doGnuplot(arg, exeName)
