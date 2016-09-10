b3b48bd9e64ebdfb67bc07a8824dc3dd26bec238
TakanoriOnuma <s1000607@sendai-nct.jp>
2016-09-10 10:49:11 +0900

    ループで複数ディレクトリに対して実行を可能にした
    doGnuplotメソッドを作成して実行ファイルが増えても
    分かり易いようにした。
    
    Signed-off-by: TakanoriOnuma <s1000607@sendai-nct.jp>

--- arrangeProgram/script.py
+++ arrangeProgram/script.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
+import os
+import glob
 import subprocess
 
-dirName = 'C:/Python27/motionLearning/arrangeProgram/part1'
+# gnuplotの実行
+def doGnuplot(arg, exeName):
+    p = subprocess.Popen(['gnuplot', '-e', arg, exeName], stderr=subprocess.PIPE)
+    # エラーがあったら出力する
+    for line in p.stderr:
+        print line,    
 
-arg = "path='{}';limit={}".format(dirName, 10000)
-exeName = 'gnuplot/error.gp'
-p = subprocess.Popen(['gnuplot', '-e', arg, exeName], stderr=subprocess.PIPE)
-for line in p.stderr:
-    print line,
+ROOT = os.getcwd()
+for dirName in glob.glob('part*'):
+    print dirName
+    # 学習エラーをグラフに表示する
+    arg = "path='{}/{}';limit={}".format(ROOT, dirName, 10000)
+    exeName = 'gnuplot/error.gp'
+    doGnuplot(arg, exeName)
+
